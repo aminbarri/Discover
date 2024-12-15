3ccd973a780acab881bca3dbec34fd53ca197064
mohamed amin barri <barrimohamed01@gmail.com>
2024-12-15 12:47:51 +0100

    type col

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Log in</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.mindd.css') }}">
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
